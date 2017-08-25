1a7de2e18f79f2cc7fd19e4cee74c931953f4706
Sam Robinson-Adams <samziz@me.com>
2017-08-25 16:14:41 +0100

    Added google translate
    Change to event listener structure
    
    Corrected accidental space
    
    Class selection changes to make sure JQ func works properly
    
    Tweaked JavaScript
    
    stuff
    
    closing bracket :(
    
    stuff
    
    Hopefully final commit
    
    Moooooore changes
    
    Searches now working
    
    Added google translate to all view
    
    Using Google translations API with API key

--- app/assets/javascripts/tolk/actions.js
+++ app/assets/javascripts/tolk/actions.js
@@ -1,13 +1,39 @@
 $(function () {
 
+  gapi.load('client');
+
   // Copy text action
-  $(".translations .actions .copy").click(function (e) {
+  $('.translations .actions .copy').click(function (e) {
     e.preventDefault();
 
     var row = $(this).parents("tr")
       , original_text = row.find(".original textarea").val();
 
     row.find(".translation textarea").val(original_text.trim()).trigger("change");
+  });
+
+  // Google Translate action (NEW)
+  $('.gtranslate').click(function (e) {
+    e.preventDefault();
+
+    var origText = $(this).parent(".actions").next('.original').find("textarea").val();
+    var destLang = $(this).data('locale');
+    var self = this;
+    gapi.client.init({
+      'apiKey': window.googleApiKey,
+      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/translate/v2/rest'],
+    }).then(function() {
+      return gapi.client.language.translations.list({
+        q: origText,
+        source: 'en',
+        target: destLang,
+      });
+    }).then(function(response) {
+      var destText = response.result.data.translations[0].translatedText;
+      $(self).parents('tr').find(".translation textarea").val(destText);
+    }, function(reason) {
+      console.warn('Error: ' + reason.result.error.message);
+    });
   });
 
   // avoid lose data
